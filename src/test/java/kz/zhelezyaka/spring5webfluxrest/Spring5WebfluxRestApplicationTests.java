0abd28b9fe471db453ca38ee0fa34ff3146417ab
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-07-27 17:21:55 +0300

    Start project

---
+++ src/test/java/kz/zhelezyaka/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.spring5webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
