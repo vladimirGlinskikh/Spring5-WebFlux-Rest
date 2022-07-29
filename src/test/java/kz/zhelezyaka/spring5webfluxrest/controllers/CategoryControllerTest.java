f5de76bf8be9e4df27cbaa3ff99ce0068107d2a3
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-07-29 15:57:49 +0300

    Testing Category Controller

---
+++ src/test/java/kz/zhelezyaka/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,49 @@
+package kz.zhelezyaka.spring5webfluxrest.controllers;
+
+import kz.zhelezyaka.spring5webfluxrest.domain.Category;
+import kz.zhelezyaka.spring5webfluxrest.repositories.CategoryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+    @Before
+    public void setUp() throws Exception {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    public void list() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(Category.builder().description("CatSasha").build(),
+                        Category.builder().description("DogVanya").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getById() {
+        BDDMockito.given(categoryRepository.findById("myid"))
+                .willReturn(Mono.just(Category.builder().description("CatSasha").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/myid")
+                .exchange()
+                .expectBody(Category.class);
+    }
+}
