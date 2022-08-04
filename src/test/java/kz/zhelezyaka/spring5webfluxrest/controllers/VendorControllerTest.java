721ddf72c61996b01aaeda7d905d10ad89fcf710
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-08-04 11:02:42 +0300

    Implement POST for Vendors

--- src/test/java/kz/zhelezyaka/spring5webfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/kz/zhelezyaka/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
 public class VendorControllerTest {
     WebTestClient webTestClient;
@@ -51,5 +53,24 @@ public class VendorControllerTest {
                 .uri("/api/v1/vendors/myId")
                 .exchange()
                 .expectBody(Vendor.class);
+    }
+
+    @Test
+    public void create() {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().build()));
+
+        Mono<Vendor> nameToSaveMono =
+                Mono.just(Vendor.builder()
+                        .firstName("Vladimir")
+                        .lastName("Glinskikh")
+                        .build());
+
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(nameToSaveMono, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
     }
 }
